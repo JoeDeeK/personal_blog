4426dd20065f4ae1719ad1876d25dce53c8538db
Jody Kirton <jody2be@yahoo.ca>
2017-10-09 12:52:24 -0400

    Delete DELETElogout.php
    no longer need this file

--- includes/DELETElogout.php
+++
@@ -1,9 +0,0 @@
-<?php require_once("includes/sessions.php"); ?>
-<?php require_once("includes/functions.php"); ?>
-
-<?php 	
-	$_SESSION["User_Id"]=null;
-	$_SESSION["User_Name"]=null;
-	session_destroy();
-	Redirect_to("index.php?Dash=false");
-?> 
